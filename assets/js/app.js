c4cd3e33bdbdf09697661e5b9b7730cf60486543
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-04-02 12:54:07 +0600

    added counter

--- assets/js/app.js
+++ assets/js/app.js
@@ -145,4 +145,20 @@
         stickyHeader();
     });
 
+    /* ========= COUNTER WIDGET ========*/
+    const counters = document.querySelectorAll(".counter");
+    counters.forEach((counter) => {
+        counter.innerText = "0";
+        const updateCounter = () => {
+            const target = +counter.getAttribute("data-target");
+            const count = +counter.innerText;
+            const increment = target / 5000;
+            if (count < target) {
+                counter.innerText = `${Math.ceil(count + increment)}`;
+                setTimeout(updateCounter, 50);
+            } else counter.innerText = target;
+        };
+        updateCounter();
+    });
+
 })(jQuery);
